990503afd47e2a85e99741d930e98d8bb8388ebd
Arpit Mallick <arpit@Arpits-Air.lan>
2023-10-22 14:30:11 -0700

    Minor changes

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const title = document.getElementById("page-title");
 console.log(title);
 
 // get elements by class
-const errors = document.getElementsByClassName("error");
+const errors = document.getElementsByClassName("error"); // Returns an array of all error class
 console.log(errors);
 
 // get elements by tag name
-const paras = document.getElementsByTagName("p");
+const paras = document.getElementsByTagName("p"); // Returns an array of all the paragraph tags
 console.log(paras);
 
 // Now the issue with these getElementByClassName & getElementsByTagName is that we can't use forEach() method with these.
