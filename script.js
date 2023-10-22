6ae72a2c7704e5da090100d4645e00be665d2c3a
Arpit Mallick <arpit@Arpits-Air.lan>
2023-10-22 16:01:55 -0700

    Initial commit

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-const header = document.querySelector("h1");
-console.log(header);
+const error = document.querySelector("p");
+console.log(error);
 
-// header.setAttribute("style", "margin: 50px;"); // Using setAttribute() it overwrites the color property. This is a big drawback of setAttribute()
-// Instead we can use .style property as below:
-console.log(header.style);
+console.log(error.classList);
+error.classList.add("error"); // add a class
+error.classList.remove("error"); //remove a class
 
-header.style.margin = '50px';
-header.style.color = 'blue';
-header.style.margin = ''; // Removes the margin
-header.style.fontSize = '50px';
+error.classList.add("success"); // add a class
